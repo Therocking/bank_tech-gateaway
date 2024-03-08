9d81e063acc72f297b3c875f13a8a331010b7201
Jose <jgenao588@gmail.com>
2024-03-08 17:48:40 -0400

    assembly files ignored

--- Bank_tech-gateaway/obj/Debug/net8.0/Bank_tech-gateaway.AssemblyInfo.cs
+++ Bank_tech-gateaway/obj/Debug/net8.0/Bank_tech-gateaway.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Bank_tech-gateaway")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bac8b5b01e284e278f28c911f05b8ce415a2eb28")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cd66e04d39a159d84c5eb7969d8628220d995148")]
 [assembly: System.Reflection.AssemblyProductAttribute("Bank_tech-gateaway")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Bank_tech-gateaway")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
